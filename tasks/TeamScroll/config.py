65dad70f6aa0f68e21457774b533a23cf15db25e
xylolit-mu <gf7004msi95@163.com>
2026-08-08 22:14:01 +0800

    feat: add TeamScroll execution time setting

--- tasks/TeamScroll/config.py
+++ tasks/TeamScroll/config.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 from pydantic import BaseModel, Field
 
-from tasks.Component.config_base import ConfigBase
+from tasks.Component.config_base import ConfigBase, Time
 from tasks.Component.config_scheduler import Scheduler
 
 
@@ -15,6 +15,11 @@ class TeamScrollConfig(BaseModel):
     teammate_config_name: str = Field(default='', description='teammate_config_name_help')
     mode: TeamScrollMode = Field(default=TeamScrollMode.LEADER, description='team_scroll_mode_help')
     realm_raid_ticket_threshold: int = Field(default=30, ge=1, le=30, description='realm_raid_ticket_threshold_help')
+    execution_time: Time = Field(
+        default=Time(minute=30),
+        title='执行时间',
+        description='team_scroll_execution_time_help',
+    )
 
 
 class TeamScroll(ConfigBase):
